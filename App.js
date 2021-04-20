da8bb240f136b6146f99b647729b26598f92c05e
Nitesh Tak -X (ntak - HCL TECHNOLOGIES LTD at Cisco) <ntak@partnet.cisco.com>
2021-04-20 10:12:07 +0530

    My first react app

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let a=1;
+  console.log("APp executed");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Nikita</Text>
       <StatusBar style="auto" />
     </View>
   );
